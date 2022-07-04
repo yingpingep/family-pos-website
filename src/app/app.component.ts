62704ddf942377d3cd35fa143d56b78a24395bff
EP Lin <pingeplin@gmail.com>
2022-07-04 16:25:13 +0800

    Setup prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'family-pos-website';
+    title = 'family-pos-website';
 }
